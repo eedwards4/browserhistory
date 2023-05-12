9af9a0b285314bfe2cd6c5235f42d0ebf5d632fd
Ethan Edwards <edwardet@sonoma.edu>
2023-05-12 11:38:26 -0700

    Added the beginnings of file handling

--- main.cpp
+++ main.cpp
@@ -8,9 +8,28 @@
 int main(int argc, char* argv[]) {
     ifstream infile; ofstream outfile;
     CommonLib::initFiles(infile, outfile, argc, argv, "BrowserHistory");
+    string word;
 
     while(!infile.eof()){
+        infile >> word;
+        if (word == "BrowserHistory"){
+            // Read in URL to list
+            
+            while (word != "endOfHistory"){
+                if (word == "visit"){ // URL
 
+                }
+                if  (word  == "browseUntil"){ // Reset link time
+
+                }
+            }
+        } else if (word == "visit"){
+
+        } else if  (word  == "browseUntil"){
+
+        } else if (word == "endOfHistory"){
+
+        }
     }
 }
 
