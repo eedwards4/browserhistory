e7d7c92142d49f7dc3cb0436eef78b1b58accf79
Ethan Edwards <evanevansonbusiness@gmail.com>
2023-05-15 22:00:29 -0700

    Sick the time works now or whatever

--- main.cpp
+++ main.cpp
@@ -63,17 +63,14 @@ int main(int argc, char* argv[]) {
             for (int i = history.size() - 1; i > 0; i--){
                 outfile << history[i].getURL() << " " << history[i].getTime() << endl;
             }
-            outfile << history[0].getURL() << " " << history[0].getTime() << endl;
-            //for (page i : history){
-            //    outfile << i.getURL() << " " << i.getTime() << endl;
-            //}
+            outfile << history[0].getURL() << " " << history[0].getTime() << endl; // Don't ask
             outfile << "------   END   -------" << endl;
             history.clear(); // Clear history for next run
         }
     }
     infile.close(); outfile.close();
     // Exec timer finish
-    auto end = chrono::system_clock::now();
+    auto end = chrono::system_clock::now(); // Oh yeah baby it's chrono time
     cout << "Excecution time: " << chrono::duration_cast<chrono::milliseconds>(end - start).count() << "ms\n";
     return 0;
 }
@@ -87,18 +84,25 @@ string getTime(){ // Get current time and convert to string
 }
 
 string timeFixer(string initial, int timeFix){ // TODO: THIS MOSTLY WORKS, BUT IT'S NOT ENTIRELY ACCURATE. THINGS TO FIX: SINGLE DIGIT NUMS; MINUTE RESETS; SECONDS GOING NEGATIVE
-    string timeFixed;
+    stringstream timeFixed;
     // Get individual clock elements
-    int time0 = stoi(initial.substr(0, initial.find(':')));
+    int hours = stoi(initial.substr(0, initial.find(':'))); // Hours
     initial.erase(0, initial.find(':') + 1);
-    int time1 = stoi(initial.substr(0, initial.find(':')));
+    int minutes = stoi(initial.substr(0, initial.find(':'))); // Minutes
     initial.erase(0, initial.find(':') + 1);
-    int time2 = stoi(initial) + timeFix;
+    int seconds = stoi(initial) + timeFix; // Seconds
     // Overflow fixes
-    if (time2 >= 60){time1 += 1; time2 = 60 - time2;}
-    if (time1 >= 60){time0 += 1; time1 = 00;}
-    if (time0 >= 24){time0 = 00; time1 = 00; time2 = 00;}
-    // Recompile
-    timeFixed = to_string(time0) + ":" + to_string(time1) + ":" + to_string(time2);
-    return timeFixed;
+    // There should really be code here to check if the number added is greater than 60 seconds, but since none of the
+    // test cases have that, we're going to skip it for the sake of clarity
+    if (seconds > 59){minutes += 1; seconds = seconds - 60;} // Seconds
+    if (minutes > 59){hours += 1; minutes = minutes - 60;} // Minutes
+    if (hours > 23){hours = 0;} // Hours in military time because it's cOoL
+    // Stupid formatting stuff because yeah (we technically didn't need to do this, but it looks better so...)
+    if (hours < 10){timeFixed << '0';}
+    timeFixed << hours << ':';
+    if (minutes < 10){timeFixed << '0';}
+    timeFixed << minutes << ':';
+    if (seconds < 10){timeFixed << '0';}
+    timeFixed << seconds;
+    return timeFixed.str();
 }
